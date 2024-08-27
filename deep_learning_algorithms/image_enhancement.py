4716a259fa30369bfae349eaa678614025234a18
Kisura W.S.P <kisura2003@gmail.com>
2024-08-27 15:24:51 +0530

    deep learning progress: added reducing noise algorithm in image_enhancement section

--- deep_learning_algorithms/image_enhancement.py
+++ deep_learning_algorithms/image_enhancement.py
@@ -1,7 +1,28 @@
 # Image Enhancement Algorithms
 
-def reducing_noise():
-    pass
+# Reducing Noise Imports
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+def reducing_noise(image_path : str):
+    img = cv.imread(image_path)
+    dst = cv.fastNlMeansDenoising(img,None,3,7,21)
+    plt.subplot(121),plt.imshow(img)
+    plt.subplot(122),plt.imshow(dst)
+    plt.show()
+
+def reducing_noise_colored(image_path : str):
+    img = cv.imread(image_path)
+    dst = cv.fastNlMeansDenoisingColored(img,None,10,10,7,21)
+    plt.subplot(121),plt.imshow(img)
+    plt.subplot(122),plt.imshow(dst)
+    plt.show()
+
+"""
+TEST CASES
+reducing_noise("noisy.jpg") # GrayScale
+reducing_noise_colored("image.png") # Colored
+"""
 
 def sharpening_image():
     pass
