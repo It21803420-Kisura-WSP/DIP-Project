bfbea5029cde3dc8ac6ae1452cf933ff3350bcd7
Kisura W.S.P <kisura2003@gmail.com>
2024-09-14 09:46:35 +0530

    (rotate function works in UI) implemented all basic functions

--- src/functions/basic_functions/color_change.py
+++ src/functions/basic_functions/color_change.py
@@ -3,10 +3,18 @@ import cv2
 def convert_image_to_grayscale(image_path):
     original_image = cv2.imread(image_path)
     gray_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+
     return gray_image
 
 def convert_image_to_bw(image_path):
     original_image = cv2.imread(image_path)
     gray_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
-    (thresh, black_n_white_image) = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
+    (_, black_n_white_image) = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
+
     return black_n_white_image
+
+def convert_image_to_color(image_path):
+    original_image = cv2.imread(image_path)
+    color_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+
+    return color_image
